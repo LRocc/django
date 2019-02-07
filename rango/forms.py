f8cc50740752608f51823a37ae165b5f5d212052
Lorenzo Roccato <2265986r@student.gla.ac.uk>
2019-02-07 13:39:42 +0000

    Final

--- rango/forms.py
+++ rango/forms.py
@@ -13,10 +13,12 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ('name',)
 
+
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200,help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -45,7 +47,6 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
-
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
