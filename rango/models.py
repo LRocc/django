2538d95add2faf991efe21442e6acd68027d5932
Lorenzo Roccato <2265986r@student.gla.ac.uk>
2019-02-07 13:40:05 +0000

    Fixes

--- rango/models.py
+++ rango/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
-
-
 class Category(models.Model):
-
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
